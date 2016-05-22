8d3e11ebe29082a50fed22abca35d73e1cc5a568
Jesus <tvalacarta@gmail.com>
2016-05-23 01:48:08 +0200

    Nuevos canales para Azteca 7 y Azteca 13 (mx)

---
+++ python/main-classic/channels/azteca7.py
@@ -0,0 +1,156 @@
+# -*- coding: utf-8 -*-
+#------------------------------------------------------------
+# tvalacarta - XBMC Plugin
+# Canal para Azteca 7 (México)
+# http://blog.tvalacarta.info/plugin-xbmc/tvalacarta/
+#------------------------------------------------------------
+import urlparse,re
+import urllib
+import os
+
+from core import config
+from core import logger
+from core import scrapertools
+from core import jsontools
+from core.item import Item
+
+DEBUG = config.get_setting("debug")
+CHANNELNAME = "azteca7"
+PROGRAMAS_URL = "http://www.azteca7.com/"
+
+def isGeneric():
+    return True
+
+def mainlist(item):
+    logger.info("tvalacarta.channels.azteca7 mainlist")
+
+    return programas(Item(channel=CHANNELNAME))
+
+def programas(item):
+    logger.info("tvalacarta.channels.azteca7 programas")
+
+    itemlist = []
+
+    if item.url=="":
+        item.url=PROGRAMAS_URL
+
+    data = scrapertools.cache_page(item.url)
+    logger.info("tvalacarta.channels.azteca7 data="+data)
+    data = scrapertools.find_single_match(data,'<span>Programas(.*?)</div[^<]+</div')
+
+    patron = '<li><a href="([^"]+)">([^<]+)</a></li>'
+    matches = re.compile(patron,re.DOTALL).findall(data)
+    if DEBUG: scrapertools.printMatches(matches)
+
+    for scrapedurl,scrapedtitle in matches:
+        title = scrapedtitle.strip()
+        thumbnail = ""
+        plot = ""
+        url = "http://www.azteca7.com/"+scrapedurl+"/historico/capitulos"
+        if (DEBUG): logger.info("title=["+title+"], url=["+url+"], thumbnail=["+thumbnail+"]")
+        itemlist.append( Item( channel=item.channel , title=title , action="episodios", url=url , thumbnail=thumbnail , plot=plot , show=title , fanart=thumbnail ) )
+
+    return itemlist
+
+def detalle_programa(item):
+
+    data = scrapertools.cache_page(item.page)
+
+    item.plot = scrapertools.find_single_match(data,'<article class="span8"[^<]+<div class="contenido_noticia">(.*?)</div>')
+    item.plot = scrapertools.htmlclean(item.plot).strip()
+
+    item.thumbnail = scrapertools.find_single_match(data,'<img src="([^"]+)" alt="" class="img-det-not">')
+
+    #item.title = scrapertools.find_single_match(data,'<article class="span8"[^<]+<h2>([^<]+)</h2>')
+
+    return item
+
+def episodios(item):
+    logger.info("tvalacarta.channels.azteca7 episodios")
+    itemlist = []
+
+    data = scrapertools.cache_page(item.url)
+
+    '''
+    <div>
+    <a href="/videos/escapeperfecto/302470/mira-nuestro-detras-de-camaras">
+    <img width="186" height="105" src="http://static.azteca.com/crop/crop.php?width=180&height=100&img=http://static.azteca.com/imagenes/2016/05/2042193--mira-nuestro-detras-de-camaras-.jpg&coordinates=50,50">
+    </a>
+    <a href="/videos/escapeperfecto/302470/mira-nuestro-detras-de-camaras"><i class="icon-play-circle"></i>¡Mira nuestro detrás de cámaras!</a>
+    <h4>2016-02-02 21:36:00 hrs</h4>
+    <p>Conoce algunas cosas curiosas que pasan detrás de las cámaras de Escape Perfecto</p>
+    </div>
+    '''
+
+    patron  = '<div[^<]+'
+    patron += '<a href="([^"]+)"[^<]+'
+    patron += '<img width="\d+" height="\d+" src="([^"]+)"[^<]+'
+    patron += '</a[^<]+'
+    patron += '<a href="[^"]+"><i[^<]+</i>([^<]+)</a[^<]+'
+    patron += '<h4>([^<]+)</h4[^<]+'
+    patron += '<p>([^<]+)</p>'
+
+    matches = re.compile(patron,re.DOTALL).findall(data)
+    if DEBUG: scrapertools.printMatches(matches)
+
+    for scrapedurl,scrapedthumbnail,scrapedtitle,fecha,scrapedplot in matches:
+        title = scrapedtitle.strip()+" "+fecha.strip()
+        thumbnail = scrapedthumbnail
+        plot = scrapedplot
+        url = urlparse.urljoin(item.url,scrapedurl)
+        if (DEBUG): logger.info("title=["+title+"], url=["+url+"], thumbnail=["+thumbnail+"]")
+        itemlist.append( Item( channel=item.channel , title=title , action="play", server="azteca", url=url , thumbnail=thumbnail , plot=plot , show=title , fanart=thumbnail, viewmode="movie_with_plot", folder=False ) )
+
+    return itemlist
+
+def detalle_episodio(item):
+
+    item.geolocked = "0"
+    
+    try:
+        from servers import azteca as servermodule
+        video_urls = servermodule.get_video_url(item.url)
+        item.media_url = video_urls[-1][1]
+    except:
+        import traceback
+        print traceback.format_exc()
+        item.media_url = ""
+
+    return item
+
+def play(item):
+
+    item.server="azteca";
+    itemlist = [item]
+
+    return itemlist
+
+# Test de canal
+# Devuelve: Funciona (True/False) y Motivo en caso de que no funcione (String)
+def test():
+    
+    # Carga el menu principal
+    items_mainlist = mainlist(Item())
+
+    # Busca el item con la lista de programas
+    items_programas = []
+    for item_mainlist in items_mainlist:
+
+        if item_mainlist.action=="programas":
+            items_programas = programas(item_mainlist)
+            break
+
+    if len(items_programas)==0:
+        return False,"No hay programas"
+
+    # Carga los episodios
+    items_episodios = episodios(items_programas[0])
+    if len(items_episodios)==0:
+        return False,"No hay episodios en "+items_programas[0].title
+
+    # Lee la URL del vídeo
+    item_episodio = detalle_episodio(items_episodios[0])
+    if item_episodio.media_url=="":
+        return False,"El conector no devuelve enlace para el vídeo "+item_episodio.title
+
+    return True,""
---
+++ python/main-classic/servers/azteca.py
@@ -0,0 +1,61 @@
+# -*- coding: utf-8 -*-
+#------------------------------------------------------------
+# pelisalacarta - XBMC Plugin
+# Conector para azteca 13 y azteca 7
+# http://blog.tvalacarta.info/plugin-xbmc/pelisalacarta/
+#------------------------------------------------------------
+
+from core import scrapertools
+from core import logger
+
+try:
+    import youtube_dl
+except:
+    from lib import youtube_dl
+
+def get_video_url( page_url , premium = False , user="" , password="", video_password="", page_data="" ):
+    logger.info("tvalacarta.servers.azteca get_video_url page_url="+page_url)
+
+    data = scrapertools.cache_page(page_url)
+    page_url = scrapertools.find_single_match(data,'<meta property="og:video" content="([^"]+)"/>')
+    logger.info("tvalacarta.servers.azteca get_video_url page_url="+page_url)
+
+    ydl = youtube_dl.YoutubeDL({'outtmpl': u'%(id)s%(ext)s'})
+    result = ydl.extract_info(page_url, download=False)
+    video_urls = []
+    if 'formats' in result:
+        for entry in result['formats']:
+            logger.info("entry="+repr(entry))
+
+            '''
+            {u'http_headers': 
+                {
+                u'Accept-Charset': u'ISO-8859-1,utf-8;q=0.7,*;q=0.7', 
+                u'Accept-Language': u'en-us,en;q=0.5', 
+                u'Accept-Encoding': u'gzip, deflate', 
+                u'Accept': u'text/html,application/xhtml+xml,application/xml;q=0.9,*/*;q=0.8', 
+                u'User-Agent': u'Mozilla/5.0 (X11; Linux x86_64; rv:10.0) Gecko/20150101 Firefox/44.0 (Chrome)'}, 
+                u'protocol': u'm3u8_native', 
+                u'format': u'hls-582 - 640x360', 
+                u'url': u'http://tvazvod-i.akamaihd.net/i/p/459791/sp/45979100/serveFlavor/entryId/0_pcso1cnx/v/2/flavorId/0_qx9uhlyx/index_0_av.m3u8', 
+                u'tbr': 582, 
+                u'height': 360, 
+                u'width': 640, 
+                u'ext': u'mp4', 
+                u'preference': None, 
+                u'format_id': u'hls-582'
+            }
+            '''
+            video_urls.append([scrapertools.safe_unicode(entry['format']).encode('utf-8'), scrapertools.safe_unicode(entry['url']).encode('utf-8')])
+            #logger.info('Append: {}'.format(entry['url']))
+
+    video_urls.reverse()
+
+    return video_urls
+
+# Encuentra vídeos del servidor en el texto pasado
+def find_videos(data):
+    encontrados = set()
+    devuelve = []
+
+    return devuelve
