82d8bda6d63b79796f81b2a054756d45b10fcdee
Jesus <tvalacarta@gmail.com>
2016-06-25 17:20:46 +0200

    Arreglado conector con Disney Channel

--- python/main-classic/servers/disneychannel.py
+++ python/main-classic/servers/disneychannel.py
@@ -10,28 +10,47 @@ import os
 
 from core import scrapertools
 from core import logger
-from core import config
-from core import jsontools
+
+from lib import youtube_dl
 
 def get_video_url( page_url , premium = False , user="" , password="", video_password="" ):
     logger.info("tvalacarta.servers.disneychannel get_video_url(page_url='%s')" % page_url)
     video_urls = []
 
     data = scrapertools.cache_page(page_url)
-    embed_url = scrapertools.find_single_match(data,'<meta itemprop="embedURL" content="([^"]+)"')
-    
-    data = scrapertools.cache_page(embed_url)
-    data = scrapertools.find_single_match(data,'<script type="text/javascript">Disney.EmbedVideo\=(.*?)\;</script>')
+    page_url = scrapertools.find_single_match(data,'"downloadUrl"\:"([^"]+)"')
+    logger.info("tvalacarta.servers.disneychannel get_video_url page_url="+page_url)
 
-    data_json = jsontools.load_json(data)
-    logger.info("tvalacarta.servers.disneychannel data_json="+repr(data_json))
+    ydl = youtube_dl.YoutubeDL({'outtmpl': u'%(id)s%(ext)s'})
+    result = ydl.extract_info(page_url, download=False)
+    video_urls = []
+    if 'formats' in result:
+        for entry in result['formats']:
+            logger.info("entry="+repr(entry))
 
-    for flavor in data_json["video"]["flavors"]:
-        media_url = flavor["url"]
-        video_urls.append( [ scrapertools.get_filename_from_url(media_url)[-4:]+" "+str(flavor["bitrate"])+"bps [disneychannel]" , media_url ] )
+            '''
+            {u'http_headers': 
+                {
+                u'Accept-Charset': u'ISO-8859-1,utf-8;q=0.7,*;q=0.7', 
+                u'Accept-Language': u'en-us,en;q=0.5', 
+                u'Accept-Encoding': u'gzip, deflate', 
+                u'Accept': u'text/html,application/xhtml+xml,application/xml;q=0.9,*/*;q=0.8', 
+                u'User-Agent': u'Mozilla/5.0 (X11; Linux x86_64; rv:10.0) Gecko/20150101 Firefox/44.0 (Chrome)'}, 
+                u'protocol': u'm3u8_native', 
+                u'format': u'hls-582 - 640x360', 
+                u'url': u'http://tvazvod-i.akamaihd.net/i/p/459791/sp/45979100/serveFlavor/entryId/0_pcso1cnx/v/2/flavorId/0_qx9uhlyx/index_0_av.m3u8', 
+                u'tbr': 582, 
+                u'height': 360, 
+                u'width': 640, 
+                u'ext': u'mp4', 
+                u'preference': None, 
+                u'format_id': u'hls-582'
+            }
+            '''
+            video_urls.append(["."+scrapertools.safe_unicode(entry['ext']).encode('utf-8'), scrapertools.safe_unicode(entry['url']).encode('utf-8')])
+            #logger.info('Append: {}'.format(entry['url']))
 
-    for video_url in video_urls:
-        logger.info("tvalacarta.servers.disneychannel %s - %s" % (video_url[0],video_url[1]))
+    video_urls.reverse()
 
     return video_urls
 
