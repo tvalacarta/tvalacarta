02e472e20d0fd19ca066413691a1ed5b459a44e3
Jesus <tvalacarta@gmail.com>
2016-11-01 01:12:23 +0100

    Fix euronews

--- python/main-classic/channels/euronews.py
+++ python/main-classic/channels/euronews.py
@@ -10,6 +10,7 @@ import urllib
 from core import logger
 from core import scrapertools
 from core.item import Item
+from core import jsontools
 
 DEBUG = False
 CHANNELNAME = "euronews"
@@ -21,69 +22,19 @@ def mainlist(item):
     logger.info("tvalacarta.euronews mainlist")
 
     itemlist = []
-    itemlist.append( Item(channel=CHANNELNAME, title="English", action="categorias", url="http://www.euronews.com") )
 
-    data = scrapertools.cache_page("http://www.euronews.com/")
+    data = scrapertools.cache_page("http://www.euronews.com/programs")
+    data = scrapertools.find_single_match(data,'<ul class="menu vertical">(.*?)</div[^<]+</li[^<]+</ul>')
     '''
     <option  dir="ltr" style="text-align:left" lang="en" value="http://www.euronews.com/">English</option>
     <option class="alt"  dir="ltr" style="text-align:left" lang="fr" value="http://fr.euronews.com/">Français</option>
     <option  dir="ltr" style="text-align:left" lang="de" value="http://de.euronews.com/">Deutsch</option>
     <option class="alt"  dir="ltr" style="text-align:left" lang="it" value="http://it.euronews.com/">Italiano</option>
     '''
-    patron = '<option.*?value="(http\://[a-z]+\.euronews\.com/)">([^<]+)</option>'
+    patron = '<a href="([^"]+)"[^>]+>([^<]+)</a>'
     matches = re.findall(patron,data,re.DOTALL)
     for url,idioma in matches:
-        itemlist.append( Item(channel=CHANNELNAME, title=idioma, action="categorias", url=url) )
-
-    return itemlist
-
-def categorias(item):
-    logger.info("tvalacarta.euronews categorias")
-    itemlist = []
-
-    # Descarga la página
-    data = scrapertools.cache_page(item.url)
-    
-    # Enlace a "Programas"
-    #<li class="menu-element-programs"><a title="Los Programas" href="/programas/">Los Programas</a></li>
-    patron = '<li class="menu-element-programs"><a title="[^"]+" href="([^"]+)">([^<]+)</a>'
-    matches = re.findall(patron,data,re.DOTALL)
-    for url,titulo in matches:
-        itemlist.append( Item(channel=CHANNELNAME, title=titulo, action="programas", url=urlparse.urljoin(item.url,url)) )
-
-    itemlist.append( Item(channel=CHANNELNAME, title="", action="videos", url="") )
-
-    # Restringe la búsqueda al bloque del menú de categorias
-    data2 = scrapertools.get_match(data,'<ol id="categoryNav">(.*?)</ol>')
-    '''
-    <ol id="categoryNav">
-    <li><a class="firstNavLink" href="/news/">News</a></li>
-    <li><a href="/business/">Business</a></li>
-    <li><a href="/sport/">Sport</a></li>
-    <li><a href="/culture/">Culture</a></li>
-    <li><a href="/nocomment/">no comment</a></li>
-    <li><a href="/european-union/">European Affairs</a></li>
-    <li><a href="/sci-tech/">Sci-tech</a></li>
-    <li><a href="/travel/">Travel</a></li>
-    <li><a href="/in-vogue/">In vogue</a></li>
-    <li id="lastNavLink"><a href="/weather/">weather</a></li>    
-    </ol>
-    '''
-    patron = '<li><a.*?href="([^"]+)">([^<]+)</a>'
-    matches = re.findall(patron,data2,re.DOTALL)
-    
-    for url,titulo in matches:
-        itemlist.append( Item(channel=CHANNELNAME, title=titulo, action="videos", url=urlparse.urljoin(item.url,url)) )
-
-    itemlist.append( Item(channel=CHANNELNAME, title="", action="videos", url="") )
-
-    # Restringe la búsqueda al bloque del menú por continentes
-    data2 = scrapertools.get_match(data,'<ol class="lhsMenu">(.*?)</ol>')
-    patron = '<li><a.*?href="([^"]+)">([^<]+)</a>'
-    matches = re.findall(patron,data2,re.DOTALL)
-    
-    for url,titulo in matches:
-        itemlist.append( Item(channel=CHANNELNAME, title=titulo, action="videos", url=urlparse.urljoin(item.url,url)) )
+        itemlist.append( Item(channel=CHANNELNAME, title=idioma, action="programas", view="programs", url=url) )
 
     return itemlist
 
@@ -94,28 +45,29 @@ def programas(item):
     data = scrapertools.cache_page(item.url)
 
     '''
-    <li id="europe-weekly">
-    <a class="imgWrap" href="/programas/europe-weekly/" title="europe weekly">
-    <img src="http://static.euronews.com/articles/programs/160x90_europe-weekly.jpg" alt="europe weekly"  title="europe weekly" />
-    <div>
-    <h2  class="programTitle">europe weekly</h2>
-    <p  class="artTitle">europe Weekly Euronews le ofrece la última hora de la actualidad económica, financiera y empresarial a nivel internacional, con los eventos de la semana.</p>
+    <div class="letter-programs">
+    <a class="program" href="http://es.euronews.com/programas/futuris">
+    <div class="program-img column medium-3 large-3 xlarge-3 small-5">
+    <img src="http://static.euronews.com/articles/programs/320x240_futuris.jpg" alt="futuris" onerror="this.onerror=null;this.src='/images/fallback-320.jpg';" />
     </div>
-    <br style="clear:both;"/>
-    </a>
-    </li>
+    <div class="program-txt column medium-7 large-7 x-large-7 small-7">
+    <h3>Futuris</h3>
+    <p>últimas noticias sobre los principales proyectos científicos y tecnológicos en Europa, descubrir los secretos de la investigación, la ciencia y la tecnología, todo ello en vídeo a la carta</p>
+    </div>
     '''
-    patron  = '<li[^<]+'
-    patron += '<a class="imgWrap" href="([^"]+)" title="([^"]+)"[^<]+'
-    patron += '<img src="([^"]+)"[^<]+'
+    patron  = '<div class="letter-programs"[^<]+'
+    patron += '<a class="program" href="([^"]+)"[^<]+'
     patron += '<div[^<]+'
-    patron += '<h2[^<]+</h2[^<]+'
-    patron += '<p\s+class="artTitle">([^<]+)</p'
+    patron += '<img src="([^"]+)"[^<]+'
+    patron += '</div[^<]+'
+    patron += '<div[^<]+'
+    patron += '<h3>([^<]+)</h3[^<]+'
+    patron += '<p>([^<]+)</p'
 
     matches = re.findall(patron,data,re.DOTALL)
     
-    for url,titulo,thumbnail,plot in matches:
-        itemlist.append( Item(channel=CHANNELNAME, title=titulo, action="videos", url=urlparse.urljoin(item.url,url), thumbnail=thumbnail, plot=plot, viewmode="movie_with_plot", fanart=thumbnail, folder=True) )
+    for url,thumbnail,titulo,plot in matches:
+        itemlist.append( Item(channel=CHANNELNAME, title=titulo, action="videos", url=urlparse.urljoin(item.url,url), thumbnail=thumbnail, plot=plot, view="videos", fanart=thumbnail, folder=True) )
 
     return itemlist
 
@@ -123,58 +75,37 @@ def videos(item):
     logger.info("tvalacarta.euronews videos")
     itemlist = []
 
-    data = scrapertools.cache_page(item.url)
-    logger.info("data="+data)
+    #http://es.euronews.com/api/program/futuris
+    #http://es.euronews.com/programas/futuris
+    data = scrapertools.cache_page(item.url.replace("/programas/","/api/program/"))
 
-    patron  = '<a class="imgLink[^"]+" title="([^"]+)" href="([^"]+)"[^<]+'
-    patron += '<span class="iconPlay"><\!-- KEEP IT FULL --></span[^<]+'
-    patron += '<img src="([^"]+)"'
-    matches = re.findall(patron,data,re.DOTALL)
+    json_objects = jsontools.load_json(data)
 
-    for scrapedtitle,scrapedurl,scrapedthumbnail in matches:
-        title = scrapedtitle
-        url = urlparse.urljoin(item.url,scrapedurl)
-        thumbnail = scrapedthumbnail
-        plot = ""
-        itemlist.append( Item(channel=CHANNELNAME, action="play", title=title, url=url, thumbnail=thumbnail, plot=plot, folder=False) )
+    for json_object in json_objects:
+        try:
+            title = json_object["title"]
+            url = json_object["canonical"]
+            thumbnail = json_object["images"][0]["url"].replace("{{w}}","960").replace("{{h}}","540")
+            plot = json_object["plainText"]
 
-    patron  = '<a class="imgLink[^"]+" title="([^"]+)" href="([^"]+)"[^<]+'
-    patron += '<img src="([^"]+)"'
-    matches = re.findall(patron,data,re.DOTALL)
+            import datetime
+            aired_date = datetime.datetime.fromtimestamp( json_object["publishedAt"] ).strftime('%Y-%m-%d %H:%M:%S')
+            logger.info("aired_date="+repr(aired_date))
 
-    for scrapedtitle,scrapedurl,scrapedthumbnail in matches:
-        title = scrapedtitle
-        url = urlparse.urljoin(item.url,scrapedurl)
-        thumbnail = scrapedthumbnail
-        plot = ""
-        itemlist.append( Item(channel=CHANNELNAME, action="play", title=title, url=url, thumbnail=thumbnail, plot=plot, folder=False) )
+            # Intenta acceder al vídeo, si no tiene deja que la excepción salte y el vídeo no se añada
+            video_element = json_object["videos"][0]
 
-    #<a href="/nocomment/2013/12/16/sudafrica-moteros-dicen-adios-a-mandela/" class="topStory"><img src="http://static.euronews.com/images_news/img_287X161_1612-south-africa-motors.jpg?1387188241" alt="Sudáfrica: moteros dicen adiós a Mandela" width="287" height="161" />
-    patron  = '<a href="(/nocomment[^"]+)" class="topStory"><img src="([^"]+)" alt="([^"]+)"'
-    matches = re.findall(patron,data,re.DOTALL)
+            try:
+                duration = json_object["videos"][0]["duration"]
+                logger.info("duration="+duration)
+                duration = scrapertools.parse_duration_secs( str(int(duration)/1000) )
+                logger.info("duration="+duration)
+            except:
+                duration = ""
 
-    for scrapedurl,scrapedthumbnail,scrapedtitle in matches:
-        title = scrapedtitle
-        url = urlparse.urljoin(item.url,scrapedurl)
-        thumbnail = scrapedthumbnail
-        plot = ""
-        itemlist.append( Item(channel=CHANNELNAME, action="play", title=title, url=url, thumbnail=thumbnail, plot=plot, folder=False) )
-
-
-    return itemlist
-
-def play(item):
-    logger.info("tvalacarta.euronews play")
-    itemlist = []
-
-    data = scrapertools.cache_page(item.url)
-    #               sources: [{file: "http://video.euronews.com/flv/nocomment/nocom-131220_NCSU_007A0-2112-_VI.flv?1387617304", label: "320p"}],
-    #http://video.euronews.com/flv/nocomment/nocom-131220_NCSU_007A0-2112-_VI.flv?1387617304
-    patron  = 'file\:\s*"([^"]+)"'
-    matches = re.findall(patron,data,re.DOTALL)
-    for url in matches:
-        mediaurl = url
-        itemlist.append( Item(channel=CHANNELNAME, title=item.title, action="play", url=mediaurl, thumbnail=item.thumbnail) )
+            itemlist.append( Item(channel=CHANNELNAME, action="play", server="euronews", title=title, url=url, thumbnail=thumbnail, plot=plot, aired_date=aired_date, duration=duration, folder=False) )
+        except:
+            logger.info("Error al cargar "+title)
 
     return itemlist
 
