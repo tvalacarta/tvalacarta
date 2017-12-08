c7e5aaf9203d684baf8740e727a5116cd67dc33f
Jesus <tvalacarta@gmail.com>
2017-12-08 19:22:43 +0100

    Arreglado conector con mitele

--- python/main-classic/servers/mitele.py
+++ python/main-classic/servers/mitele.py
@@ -5,9 +5,10 @@
 # http://blog.tvalacarta.info/plugin-xbmc/tvalacarta/
 #------------------------------------------------------------
 
-
 from core import logger
 from core import scrapertools
+from core import jsontools
+
 from lib import youtube_dl
 
 def get_video_url( page_url , premium = False , user="" , password="", video_password="" ):
@@ -19,29 +20,34 @@ def get_video_url( page_url , premium = False , user="" , password="", video_pas
     result = ydl.extract_info(page_url, download=False)
     logger.info("tvalacarta.server.mitele get_video_url result="+repr(result))
 
-    if "ext" in result and "url" in result:
-        for entries in result["formats"]:
-            if entries["ext"] != "rtmp":
-                video_url = scrapertools.safe_unicode(entries['url']).encode('utf-8')
-                video_url = video_url.replace("http://ignore.mediaset.es", "http://miteleooyala-a.akamaihd.net")
-                if entries["ext"] != "mp4":
-                    title = scrapertools.safe_unicode(entries["format"]).encode('utf-8')
-                elif entries["ext"] == "mp4":
-                    if entries.has_key("vbr"):
-                        title = "mp4-" + scrapertools.safe_unicode(str(entries["vbr"])).encode('utf-8') + " " + scrapertools.safe_unicode(entries["format"]).encode('utf-8').rsplit("-",1)[1]
-                    else:
-                        title = scrapertools.safe_unicode(entries["format"]).encode('utf-8')
+    for entries in result["formats"]:
 
+        if entries["ext"] != "rtmp":
+            video_url = scrapertools.safe_unicode(entries['url']).encode('utf-8')
+            video_url = video_url.replace("http://ignore.mediaset.es", "http://miteleooyala-a.akamaihd.net")
+
+            if entries["ext"] != "mp4":
+                title = scrapertools.safe_unicode(entries["format"]).encode('utf-8')
+
+            elif entries["ext"] == "mp4":
+
+                if entries.has_key("vbr"):
+                    title = "mp4-" + scrapertools.safe_unicode(str(entries["vbr"])).encode('utf-8') + " " + scrapertools.safe_unicode(entries["format"]).encode('utf-8').rsplit("-",1)[1]
+                else:
+                    title = scrapertools.safe_unicode(entries["format"]).encode('utf-8')
+
+            try:
+                calidad = int(scrapertools.safe_unicode(str(entries["vbr"])))
+            except:
                 try:
-                    calidad = int(scrapertools.safe_unicode(str(entries["vbr"])))
+                    calidad = int(title.split("-")[1].strip())
                 except:
-                    try:
-                        calidad = int(title.split("-")[1].strip())
-                    except:
-                        calidad = 3000
-                video_urls.append(["%s" % title, video_url, 0, False, calidad])
-                
+                    calidad = 3000
+
+            video_urls.append(["%s" % title, video_url, 0, False, calidad])
+            
     video_urls.sort(key=lambda video_urls: video_urls[4], reverse=True)
+
     for url in video_urls:
         logger.info("[mitele.py] %s - %s" % (url[0],url[1]))
 
